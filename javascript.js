e3ef7ec35c1df02b26d1da676beaa41f8d477623
D4v1D-lab <deividfrend@gmail.com>
2020-01-15 18:19:57 -0700

    commit

--- javascript.js
+++ javascript.js
@@ -5,3 +5,8 @@ const currentYear = currentDate.getFullYear();
 console.log(currentYear);
 
 document.getElementById("currentYear").innerHTML = currentYear;
+
+const string = document.lastModified;
+
+console.log(string);
+document.getElementById("lastModification").innerHTML = string;
