0ad20e86e003ebab7413e4579bfdb2bc0803fd79
D4v1D-lab <deividfrend@gmail.com>
2020-02-08 23:52:58 -0700

    commit

--- lesson5/scripts/preston-5.js
+++ lesson5/scripts/preston-5.js
@@ -1,8 +1,50 @@
 
+var currentDate = new Date();
+var currentDateString;
 
+// get day of week
+var weekDayNumber = currentDate.getDay();
 
+var daysOfWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday'
+];
 
+var weekDay = daysOfWeek[weekDayNumber];
 
+currentDateString = weekDay + ', ';
+
+currentDateString += currentDate.getDate();
+
+var months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+];
+
+var monthNumber = currentDate.getMonth();
+
+var month = months[monthNumber];
+
+currentDateString += ' ' + month;
+
+currentDateString += ' ' + currentDate.getFullYear();
+
+document.getElementById('currentDate').innerHTML = currentDateString; /*display date*/
 
 document.querySelector('#current-temp').innerHTML = '50&deg;'; // 
 document.querySelector('#wind-speed').innerHTML = '10mph';
