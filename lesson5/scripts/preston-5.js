8b02c18cd616f92a2761d5e1585ee412a8438368
D4v1D-lab <deividfrend@gmail.com>
2020-02-08 23:30:14 -0700

    commit

--- lesson5/scripts/preston-5.js
+++ lesson5/scripts/preston-5.js
@@ -1,3 +1,9 @@
+
+
+
+
+
+
 document.querySelector('#current-temp').innerHTML = '50&deg;'; // 
 document.querySelector('#wind-speed').innerHTML = '10mph';
 document.querySelector('#humidity').innerHTML = '10&percnt;';
