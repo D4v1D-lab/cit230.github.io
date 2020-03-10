dbea8cf6ffd1ce516d2e93fa7688598de623d08d
D4v1D-lab <deividfrend@gmail.com>
2020-03-10 10:46:33 -0600

    commit

--- lesson10/scripts/api.js
+++ lesson10/scripts/api.js
@@ -3,10 +3,29 @@ const apiURL = "htpss://api.openweathermap.org/data/2.5/weather?id=2172797"
 fetch(apiURL)
 .then(
     response => {
-        response = response.json{}
+        return response.json()
     }
 )
-.thenjsObject =>{
+.then(
+    jsObject => {
 console.log(jsObject);
+document.querySelector('#current-temp')
+.textContent = jsObject.main.temp;
+
+let image = 
+'htpps'
+jsObject.weather[0].icon +
+'.png';
+
+document.querySelector('#weather-icon')
+    .setAttribute('src', image);
+
+    let alt = jsObject.wether[0].description
+document.querySelector('#weather-icon')
+    .setAttribute('alt', alt);
+
+    /*optimized*/
+
+
 }
 );
