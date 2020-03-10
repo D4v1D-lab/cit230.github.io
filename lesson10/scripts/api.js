26c96e0f3dabe8bcb71570909b097a5154fcbe8a
D4v1D-lab <deividfrend@gmail.com>
2020-03-10 10:37:27 -0600

    commit

---
+++ lesson10/scripts/api.js
@@ -0,0 +1,12 @@
+const apiURL = "htpss://api.openweathermap.org/data/2.5/weather?id=2172797"
+
+fetch(apiURL)
+.then(
+    response => {
+        response = response.json{}
+    }
+)
+.thenjsObject =>{
+console.log(jsObject);
+}
+);
